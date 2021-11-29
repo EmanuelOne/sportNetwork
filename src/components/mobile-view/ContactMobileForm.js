f86b446d29203e62509f64073983dabdf74bf0de
emanuelone <emanuel4mechie@gmail.com>
2021-11-29 13:54:27 +0100

    Mergin

--- src/components/mobile-view/ContactMobileForm.js
+++ src/components/mobile-view/ContactMobileForm.js
@@ -1,17 +1,9 @@
 import { Button, makeStyles, TextField } from '@material-ui/core';
-<<<<<<< HEAD
 import React, { useState } from 'react';
-=======
-import React, {useState} from  'react';
->>>>>>> 856bf54dd573c48e86220641ecf1e69807504d5f
 import SendIcon from '@mui/icons-material/Send';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import { Box } from '@mui/system';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 856bf54dd573c48e86220641ecf1e69807504d5f
 import { ContactInformation } from './contactInformation';
 import { ContactIcons } from './contactIcons';
 import { MenuBar } from './menuBar';
@@ -19,7 +11,6 @@ import { ImageView } from './imageView';
 import { Typography } from '@mui/material';
 
 const useStyles = makeStyles({
-<<<<<<< HEAD
   contactForm: {
     width: '100%',
     alignItems: 'center',
@@ -153,163 +144,6 @@ export const ContactMobileForm = (props) => {
       <Box>
         <ContactIcons />
       </Box>
-      <div style={{ clipPath: 'circle(50.0% at 0 51%)' }}></div>
     </Box>
   );
 };
-=======
-    contactForm:{
-        width:"100%",
-        alignItems:'center',
-
-
-    },
-    txtField:{
-        marginTop:"2em",
-        fontFamily:'Poppins',
-        color:'rgba(0, 0, 0, 0.5)',
-        
-    },
-    btn:{
-        backgroundColor:'#2F2C2C',        
-        lineHeight: '24px',        
-        width:'234px',
-        height:'55px',
-        color:'#FFFFFF',
-        
-        justifyContent:'center',
-        '&:hover':{
-            backgroundColor:'#2F2C2C'
-
-        }
-
-
-    },
-    cIcon:{
-        color:'#FCE61D',
-        paddingLeft:'1em',
-        width:'30px',
-        height:'20px'
-    },
-    txt:{
-        color:'#201F1E',
-        fontFamily:'Poppins',
-        padding:'1em'
-        
-        
-    },
-    rectangle:{
-        
-        height:'70px',
-        backgroundColor:'#F9E31D',
-        height:'100px'
-        
-    },
-    txt1:{
-        
-        fontFamily:'Poppins',
-        padding:'1em'
-
-    },
-    input:{
-        color:'green'
-    }
-
-
-})
-
-const initialFormValues = {
-    fullName:'',
-    address:'',
-    phone:'',
-    message:''
-
-}
- export const ContactMobileForm = (props)=>{
-     const [formValue,setFormValue] = useState(initialFormValues);
-     const classes = useStyles();
-   const handleChange = (e)=>{
-        const {name,value} = e.target;
-        
-         setFormValue({
-             ...formValue,
-             [name]:value,
-         })
-
-     }
-   const  handleSubmit = (e)=>{
-       e.preventDefault();
-         console.log("data");
-         console.log(formValue);
-     }
-    return (
-        <Box className = {classes.contactForm}>
-
-            
-            <ImageView />
-            <Box className = {classes.rectangle}>
-            
-            </Box>
-            <Box>
-                <Typography className = {classes.txt}>
-                    We won't be sharing any of your information with any one. We are completely at your service
-                </Typography>
-            </Box>
-
-           <Box className = {classes.txt1}>
-           <TextField className = {classes.txtField} 
-           label="Full Name" 
-           name = "fullName"
-            value = {formValue.fullName}
-            onChange = {handleChange}
-
-           fullWidth />
-           <TextField 
-           className = {classes.txtField}            
-           label="Contact Address"  
-           name = "address"
-           value = {formValue.address}
-           onChange = {handleChange}
-
-           fullWidth />
-           <TextField 
-           className = {classes.txtField} 
-           label="Phone"  
-           name = "phone"
-           value = {formValue.phone}
-           onChange = {handleChange}
-
-           fullWidth />
-           <TextField className = {classes.txtField}
-           name = "message"
-           value = {formValue.message}
-           onChange = {handleChange}
-            multiline
-            rows={10}
-            variant="filled"
-            placeholder = "Type your message here"
-            inputProps = {{className:classes.input}}
-            fullWidth
-            />
-            <Box textAlign = "center">
-                <Button 
-                className = {`${classes.txtField} ${classes.btn}`} 
-                
-                onClick = {handleSubmit}
-                endIcon={<TelegramIcon className = {classes.cIcon}/>}>
-                    SEND MESSAGE 
-                </Button>
-            </Box>
-      </Box>
-
-      <Box>
-          < ContactInformation />
-      </Box>
-           <Box>
-               <ContactIcons />
-               </Box>
-        </Box>
-    )
-}
-
->>>>>>> 856bf54dd573c48e86220641ecf1e69807504d5f
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
 import { ContactDesktopForm } from '../../components/desktop-view/contactDesktopForm';
 
 import { ContactMobileForm } from '../../components/mobile-view/ContactMobileForm';
-=======
-
-import { ContactDesktopForm } from "../../components/desktop-view/contactDesktopForm";
-
-import { ContactMobileForm } from "../../components/mobile-view/ContactMobileForm";
-
->>>>>>> 856bf54dd573c48e86220641ecf1e69807504d5f
 
 function Contact() {
   // The current width of the viewport
   const width = window.innerWidth;
   // The width below which the mobile view should be rendered
   const breakpoint = 620;
-<<<<<<< HEAD
   console.log(width < breakpoint);
 
   /* If the viewport is more narrow than the breakpoint render the
@@ -24,13 +15,3 @@ function Contact() {
 }
 
 export default Contact;
-=======
-  
-  /* If the viewport is more narrow than the breakpoint render the
-     mobile component, else render the desktop component */
-  return width < breakpoint ? <ContactMobileForm /> : <ContactDesktopForm />;
-  
-}
-
-export default Contact;
->>>>>>> 856bf54dd573c48e86220641ecf1e69807504d5f
